e8b4cd6e4540c848e531e7e70c8c4ecc8193a049
Marcus.sze <marcus.sze@jervoisventures.comntures.com>
2025-03-12 12:43:41 +0800

    👌 IMPROVE: Refactor UI

--- src/components/release.jsx
+++ src/components/release.jsx
@@ -10,70 +10,72 @@ function Release (props) {
 
     return (
         <div className="release">
-        <label htmlFor="draw" className='caveat-400'>{id+1}</label> &nbsp;&nbsp;
-        <input
-          type="text"
-          id="termNum"
-          name="termNum"
-          value={release[0]}
-          onChange={(event) => updateRelease(id, 0, event.target.value)}
-        />{" "}
-        &nbsp;
-        <input
-          type="text"
-          id="releaseNum1"
-          name="releaseNum1"
-          value={release[1]}
-          onChange={(event) => updateRelease(id, 1, event.target.value)}
-        />
-        -
-        <input
-          type="text"
-          id="releaseNum2"
-          name="releaseNum2"
-          value={release[2]}
-          onChange={(event) => updateRelease(id, 2, event.target.value)}
-        />
-        -
-        <input
-          type="text"
-          id="releaseNum3"
-          name="releaseNum3"
-          value={release[3]}
-          onChange={(event) => updateRelease(id, 3, event.target.value)}
-        />
-        -
-        <input
-          type="text"
-          id="releaseNum4"
-          name="releaseNum4"
-          value={release[4]}
-          onChange={(event) => updateRelease(id, 4, event.target.value)}
-        />
-        -
-        <input
-          type="text"
-          id="releaseNum5"
-          name="releaseNum5"
-          value={release[5]}
-          onChange={(event) => updateRelease(id, 5, event.target.value)}
-        />
-        -
-        <input
-          type="text"
-          id="releaseNum6"
-          name="releaseNum6"
-          value={release[6]}
-          onChange={(event) => updateRelease(id, 6, event.target.value)}
-        />
-        &nbsp; / &nbsp;
-        <input
-          type="text"
-          id="releaseNum7"
-          name="releaseNum7"
-          value={release[7]}
-          onChange={(event) => updateRelease(id, 7, event.target.value)}
-        />
+        <label htmlFor="draw" className='caveat-400'>{id + 1}</label> &nbsp;&nbsp;
+        <div className="input-container">
+            <input
+            type="text"
+            id="termNum"
+            name="termNum"
+            value={release[0]}
+            onChange={(event) => updateRelease(id, 0, event.target.value)}
+            />{" "}
+            &nbsp;
+            <input
+            type="text"
+            id="releaseNum1"
+            name="releaseNum1"
+            value={release[1]}
+            onChange={(event) => updateRelease(id, 1, event.target.value)}
+            />
+            -
+            <input
+            type="text"
+            id="releaseNum2"
+            name="releaseNum2"
+            value={release[2]}
+            onChange={(event) => updateRelease(id, 2, event.target.value)}
+            />
+            -
+            <input
+            type="text"
+            id="releaseNum3"
+            name="releaseNum3"
+            value={release[3]}
+            onChange={(event) => updateRelease(id, 3, event.target.value)}
+            />
+            -
+            <input
+            type="text"
+            id="releaseNum4"
+            name="releaseNum4"
+            value={release[4]}
+            onChange={(event) => updateRelease(id, 4, event.target.value)}
+            />
+            -
+            <input
+            type="text"
+            id="releaseNum5"
+            name="releaseNum5"
+            value={release[5]}
+            onChange={(event) => updateRelease(id, 5, event.target.value)}
+            />
+            -
+            <input
+            type="text"
+            id="releaseNum6"
+            name="releaseNum6"
+            value={release[6]}
+            onChange={(event) => updateRelease(id, 6, event.target.value)}
+            />
+            &nbsp; / &nbsp;
+            <input
+            type="text"
+            id="releaseNum7"
+            name="releaseNum7"
+            value={release[7]}
+            onChange={(event) => updateRelease(id, 7, event.target.value)}
+            />
+        </div>
         &nbsp;&nbsp;
         <DeleteOutlined onClick={() => deleteRelease(id)} className="delete-btn" />
       </div>
