cbd0593d81dbd41325b93fc35db7cd698a2acd21
marcus.sze <szeyulam1028@gmail.com>
2025-03-13 00:09:01 +0800

    👌 IMPROVE: Refactor UI

--- src/components/draw.jsx
+++ src/components/draw.jsx
@@ -10,64 +10,78 @@ function Draw (props) {
 
     return (
       <div className="draw">
-        <label htmlFor="draw" className='caveat-400'>{id + 1}</label> &nbsp;&nbsp;
-        <div className='input-container'>    
-            <input
+        <label htmlFor="draw" className="caveat-400">
+          {id + 1}&nbsp;&nbsp;
+        </label>
+        <div className="input-container">
+          <input
             type="tel"
             id="drawNum1"
             name="drawNum1"
-                    value={draw[0]}
-                    ref={(el) => {inputRef.current[id][0] = el}}
+            value={draw[0]}
+            ref={(el) => {
+              inputRef.current[id][0] = el;
+            }}
             onChange={(event) => updateDraw(id, 0, event.target.value)}
-                />
-            -
-            <input
+          />
+          -
+          <input
             type="tel"
             id="drawNum2"
             name="drawNum2"
-                    value={draw[1]}
-                    ref={(el) => {inputRef.current[id][1] = el}}
+            value={draw[1]}
+            ref={(el) => {
+              inputRef.current[id][1] = el;
+            }}
             onChange={(event) => updateDraw(id, 1, event.target.value)}
-            />
-            -
-            <input
+          />
+          -
+          <input
             type="tel"
             id="drawNum3"
             name="drawNum3"
-                    value={draw[2]}
-                    ref={(el) => {inputRef.current[id][2] = el}}
+            value={draw[2]}
+            ref={(el) => {
+              inputRef.current[id][2] = el;
+            }}
             onChange={(event) => updateDraw(id, 2, event.target.value)}
-            />
-            -
-            <input
+          />
+          -
+          <input
             type="tel"
             id="drawNum4"
             name="drawNum4"
-                    value={draw[3]}
-                    ref={(el) => {inputRef.current[id][3] = el}}
+            value={draw[3]}
+            ref={(el) => {
+              inputRef.current[id][3] = el;
+            }}
             onChange={(event) => updateDraw(id, 3, event.target.value)}
-            />
-            -
-            <input
+          />
+          -
+          <input
             type="tel"
             id="drawNum5"
             name="drawNum5"
-                    value={draw[4]}
-                    ref={(el) => {inputRef.current[id][4] = el}}
+            value={draw[4]}
+            ref={(el) => {
+              inputRef.current[id][4] = el;
+            }}
             onChange={(event) => updateDraw(id, 4, event.target.value)}
-            />
-            -
-            <input
+          />
+          -
+          <input
             type="tel"
             id="drawNum6"
             name="drawNum6"
-                    value={draw[5]}
-                    ref={(el) => {inputRef.current[id][5] = el}}
+            value={draw[5]}
+            ref={(el) => {
+              inputRef.current[id][5] = el;
+            }}
             onChange={(event) => updateDraw(id, 5, event.target.value)}
-            />
+          />
         </div>
         &nbsp;&nbsp;
-        <DeleteOutlined onClick={() => deleteDraw(id)} className='delete-btn'/>
+        <DeleteOutlined onClick={() => deleteDraw(id)} className="delete-btn" />
       </div>
     );
     
--- src/components/release.jsx
+++ src/components/release.jsx
@@ -9,83 +9,104 @@ function Release (props) {
     const release = releases[id];
 
     return (
-        <div className="release">
-        <label htmlFor="draw" className='caveat-400'>{id + 1}</label> &nbsp;&nbsp;
+      <div className="release">
+        <label htmlFor="draw" className="caveat-400">
+          {id + 1}&nbsp;&nbsp;
+        </label>
         <div className="input-container">
-            <input
+          <input
             type="tel"
             id="termNum"
             name="termNum"
-                    value={release[0]}
-                    ref={(el) => {releaseInputRef.current[id][0] = el}}
+            value={release[0]}
+            ref={(el) => {
+              releaseInputRef.current[id][0] = el;
+            }}
             onChange={(event) => updateRelease(id, 0, event.target.value)}
-            />{" "}
-            &nbsp;
-            <input
+          />{" "}
+          &nbsp;
+          <input
             type="tel"
             id="releaseNum1"
             name="releaseNum1"
-                    value={release[1]}
-                    ref={(el) => {releaseInputRef.current[id][1] = el}}
+            value={release[1]}
+            ref={(el) => {
+              releaseInputRef.current[id][1] = el;
+            }}
             onChange={(event) => updateRelease(id, 1, event.target.value)}
-            />
-            -
-            <input
+          />
+          -
+          <input
             type="tel"
             id="releaseNum2"
             name="releaseNum2"
-                    value={release[2]}
-                    ref={(el) => {releaseInputRef.current[id][2] = el}}
+            value={release[2]}
+            ref={(el) => {
+              releaseInputRef.current[id][2] = el;
+            }}
             onChange={(event) => updateRelease(id, 2, event.target.value)}
-            />
-            -
-            <input
+          />
+          -
+          <input
             type="tel"
             id="releaseNum3"
             name="releaseNum3"
-                    value={release[3]}
-                    ref={(el) => {releaseInputRef.current[id][3] = el}}
+            value={release[3]}
+            ref={(el) => {
+              releaseInputRef.current[id][3] = el;
+            }}
             onChange={(event) => updateRelease(id, 3, event.target.value)}
-            />
-            -
-            <input
+          />
+          -
+          <input
             type="tel"
             id="releaseNum4"
             name="releaseNum4"
-                    value={release[4]}
-                    ref={(el) => {releaseInputRef.current[id][4] = el}}
+            value={release[4]}
+            ref={(el) => {
+              releaseInputRef.current[id][4] = el;
+            }}
             onChange={(event) => updateRelease(id, 4, event.target.value)}
-            />
-            -
-            <input
+          />
+          -
+          <input
             type="tel"
             id="releaseNum5"
             name="releaseNum5"
-                    value={release[5]}
-                    ref={(el) => {releaseInputRef.current[id][5] = el}}
+            value={release[5]}
+            ref={(el) => {
+              releaseInputRef.current[id][5] = el;
+            }}
             onChange={(event) => updateRelease(id, 5, event.target.value)}
-            />
-            -
-            <input
+          />
+          -
+          <input
             type="tel"
             id="releaseNum6"
             name="releaseNum6"
-                    value={release[6]}
-                    ref={(el) => {releaseInputRef.current[id][6] = el}}
+            value={release[6]}
+            ref={(el) => {
+              releaseInputRef.current[id][6] = el;
+            }}
             onChange={(event) => updateRelease(id, 6, event.target.value)}
-            />
-            &nbsp; / &nbsp;
-            <input
+          />
+          &nbsp;
+          <input
             type="tel"
             id="releaseNum7"
             name="releaseNum7"
-                    value={release[7]}
-                    ref={(el) => {releaseInputRef.current[id][7] = el}}
+            value={release[7]}
+            ref={(el) => {
+              releaseInputRef.current[id][7] = el;
+            }}
             onChange={(event) => updateRelease(id, 7, event.target.value)}
-            />
+          />
         </div>
         &nbsp;&nbsp;
-        <DeleteOutlined onClick={() => deleteRelease(id)} className="delete-btn" />
+        <DeleteOutlined
+          onClick={() => deleteRelease(id)}
+          className="delete-btn"
+        />
       </div>
     );
 }
